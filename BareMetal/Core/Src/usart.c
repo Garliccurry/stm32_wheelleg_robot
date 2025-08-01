08f6873e766a1230932c1cc7099fd1cc8b155f9a
Garliccurry <1095533196@qq.com>
2025-08-01 21:32:30 +0800

    add log system

--- BareMetal/Core/Src/usart.c
+++ BareMetal/Core/Src/usart.c
@@ -32,246 +32,270 @@ UART_HandleTypeDef huart2;
 void MX_USART1_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART1_Init 0 */
+    /* USER CODE BEGIN USART1_Init 0 */
 
-  /* USER CODE END USART1_Init 0 */
+    /* USER CODE END USART1_Init 0 */
 
-  /* USER CODE BEGIN USART1_Init 1 */
+    /* USER CODE BEGIN USART1_Init 1 */
 
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
+    /* USER CODE END USART1_Init 1 */
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN USART1_Init 2 */
 
-  /* USER CODE END USART1_Init 2 */
+    /* USER CODE END USART1_Init 2 */
 }
 /* USART2 init function */
 
 void MX_USART2_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART2_Init 0 */
+    /* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END USART2_Init 0 */
+    /* USER CODE END USART2_Init 0 */
 
-  /* USER CODE BEGIN USART2_Init 1 */
+    /* USER CODE BEGIN USART2_Init 1 */
 
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 1000000;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
+    /* USER CODE END USART2_Init 1 */
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 1000000;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN USART2_Init 2 */
 
-  /* USER CODE END USART2_Init 2 */
+    /* USER CODE END USART2_Init 2 */
 }
 
 void HAL_UART_MspInit(UART_HandleTypeDef *uartHandle)
 {
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if (uartHandle->Instance == USART1)
-  {
-    /* USER CODE BEGIN USART1_MspInit 0 */
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if (uartHandle->Instance == USART1)
+    {
+        /* USER CODE BEGIN USART1_MspInit 0 */
 
-    /* USER CODE END USART1_MspInit 0 */
-    /* USART1 clock enable */
-    __HAL_RCC_USART1_CLK_ENABLE();
+        /* USER CODE END USART1_MspInit 0 */
+        /* USART1 clock enable */
+        __HAL_RCC_USART1_CLK_ENABLE();
 
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**USART1 GPIO Configuration
-    PA9     ------> USART1_TX
-    PA10     ------> USART1_RX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**USART1 GPIO Configuration
+        PA9     ------> USART1_TX
+        PA10     ------> USART1_RX
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    /* USART1 interrupt Init */
-    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(USART1_IRQn);
-    /* USER CODE BEGIN USART1_MspInit 1 */
+        /* USART1 interrupt Init */
+        HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(USART1_IRQn);
+        /* USER CODE BEGIN USART1_MspInit 1 */
 
-    /* USER CODE END USART1_MspInit 1 */
-  }
-  else if (uartHandle->Instance == USART2)
-  {
-    /* USER CODE BEGIN USART2_MspInit 0 */
+        /* USER CODE END USART1_MspInit 1 */
+    }
+    else if (uartHandle->Instance == USART2)
+    {
+        /* USER CODE BEGIN USART2_MspInit 0 */
 
-    /* USER CODE END USART2_MspInit 0 */
-    /* USART2 clock enable */
-    __HAL_RCC_USART2_CLK_ENABLE();
+        /* USER CODE END USART2_MspInit 0 */
+        /* USART2 clock enable */
+        __HAL_RCC_USART2_CLK_ENABLE();
 
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**USART2 GPIO Configuration
-    PA2     ------> USART2_TX
-    PA3     ------> USART2_RX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**USART2 GPIO Configuration
+        PA2     ------> USART2_TX
+        PA3     ------> USART2_RX
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    /* USER CODE BEGIN USART2_MspInit 1 */
+        /* USER CODE BEGIN USART2_MspInit 1 */
 
-    /* USER CODE END USART2_MspInit 1 */
-  }
+        /* USER CODE END USART2_MspInit 1 */
+    }
 }
 
 void HAL_UART_MspDeInit(UART_HandleTypeDef *uartHandle)
 {
 
-  if (uartHandle->Instance == USART1)
-  {
-    /* USER CODE BEGIN USART1_MspDeInit 0 */
+    if (uartHandle->Instance == USART1)
+    {
+        /* USER CODE BEGIN USART1_MspDeInit 0 */
 
-    /* USER CODE END USART1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_USART1_CLK_DISABLE();
+        /* USER CODE END USART1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_USART1_CLK_DISABLE();
 
-    /**USART1 GPIO Configuration
-    PA9     ------> USART1_TX
-    PA10     ------> USART1_RX
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9 | GPIO_PIN_10);
+        /**USART1 GPIO Configuration
+        PA9     ------> USART1_TX
+        PA10     ------> USART1_RX
+        */
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9 | GPIO_PIN_10);
 
-    /* USART1 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(USART1_IRQn);
-    /* USER CODE BEGIN USART1_MspDeInit 1 */
+        /* USART1 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(USART1_IRQn);
+        /* USER CODE BEGIN USART1_MspDeInit 1 */
 
-    /* USER CODE END USART1_MspDeInit 1 */
-  }
-  else if (uartHandle->Instance == USART2)
-  {
-    /* USER CODE BEGIN USART2_MspDeInit 0 */
+        /* USER CODE END USART1_MspDeInit 1 */
+    }
+    else if (uartHandle->Instance == USART2)
+    {
+        /* USER CODE BEGIN USART2_MspDeInit 0 */
 
-    /* USER CODE END USART2_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_USART2_CLK_DISABLE();
+        /* USER CODE END USART2_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_USART2_CLK_DISABLE();
 
-    /**USART2 GPIO Configuration
-    PA2     ------> USART2_TX
-    PA3     ------> USART2_RX
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2 | GPIO_PIN_3);
+        /**USART2 GPIO Configuration
+        PA2     ------> USART2_TX
+        PA3     ------> USART2_RX
+        */
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2 | GPIO_PIN_3);
 
-    /* USER CODE BEGIN USART2_MspDeInit 1 */
+        /* USER CODE BEGIN USART2_MspDeInit 1 */
 
-    /* USER CODE END USART2_MspDeInit 1 */
-  }
+        /* USER CODE END USART2_MspDeInit 1 */
+    }
 }
 
 /* USER CODE BEGIN 1 */
 uint8_t g_rx_buf[RX_BUF_SIZE];
 static uint8_t g_command[RX_BUF_SIZE];
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+PUTCHAR_PROTOTYPE
+{
+    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
+
+    return ch;
+}
+
+int _write(int file, char *ptr, int len)
+{
+    int DataIdx;
+
+    for (DataIdx = 0; DataIdx < len; DataIdx++)
+    {
+        __io_putchar(*ptr++);
+    }
+    return len;
+}
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 {
-  if (huart->Instance == USART1)
-  {
-    if (Size == RX_BUF_SIZE)
+    if (huart->Instance == USART1)
     {
-      for (int i = 0; i < RX_BUF_SIZE; i++)
-      {
-        g_command[i] = g_rx_buf[i];
-      }
-      g_flag_usartrec = 1;
+        if (Size == RX_BUF_SIZE)
+        {
+            for (int i = 0; i < RX_BUF_SIZE; i++)
+            {
+                g_command[i] = g_rx_buf[i];
+            }
+            g_flag_usartrec = 1;
+        }
+        HAL_UARTEx_ReceiveToIdle_IT(huart, g_rx_buf, RX_BUF_SIZE);
     }
-    HAL_UARTEx_ReceiveToIdle_IT(huart, g_rx_buf, RX_BUF_SIZE);
-  }
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
 {
-  if (huart->Instance == USART1)
-  {
-  }
+    if (huart->Instance == USART1)
+    {
+    }
 }
 
-int fputc(int ch, FILE *f)
+void Usart_LogPrint(uint8_t *ch, uint16_t len)
 {
-  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 10);
-  return ch;
+    HAL_UART_Transmit(&huart1, ch, len, 100);
 }
 
 void vUart_ParseCommand(void)
 {
-  if (g_flag_usartrec == 1)
-  {
-    if (g_command[0] == 0x31)
+    if (g_flag_usartrec == 1)
     {
-      pos_left += 6;
+        if (g_command[0] == 0x31)
+        {
+            pos_left += 6;
+        }
+        else if (g_command[3] == 0x32)
+        {
+            g_vel += 10.f;
+            printf("%f\r\n", g_vel);
+        }
+        else if (g_command[3] == 0x33)
+        {
+            g_vel -= 10.f;
+            printf("%f\r\n", g_vel);
+        }
+        else if (g_command[3] == 0x34)
+        {
+            g_hight += 10;
+            printf("%d\r\n", g_hight);
+        }
+        else if (g_command[3] == 0x35)
+        {
+            g_hight -= 10;
+            printf("%d\r\n", g_hight);
+        }
+        else if (g_command[3] == 0x36)
+        {
+            wheel_run = ~wheel_run;
+        }
+        g_flag_usartrec = 0;
     }
-    else if (g_command[3] == 0x32)
-    {
-      g_vel += 10.f;
-      printf("%f\r\n", g_vel);
-    }
-    else if (g_command[3] == 0x33)
-    {
-      g_vel -= 10.f;
-      printf("%f\r\n", g_vel);
-    }
-    else if (g_command[3] == 0x34)
-    {
-      g_hight += 10;
-      printf("%d\r\n", g_hight);
-    }
-    else if (g_command[3] == 0x35)
-    {
-      g_hight -= 10;
-      printf("%d\r\n", g_hight);
-    }
-    else if (g_command[3] == 0x36)
-    {
-      wheel_run = ~wheel_run;
-    }
-    g_flag_usartrec = 0;
-  }
 }
 
 void vFTUart_Send(uint8_t *nDat, int nLen)
 {
-  HAL_UART_Transmit(&huart2, nDat, nLen, 100);
+    HAL_UART_Transmit(&huart2, nDat, nLen, 100);
 }
 
 int lFTUart_Read(uint8_t *nDat, int nLen)
 {
-  if (HAL_OK != HAL_UART_Receive(&huart2, nDat, nLen, 100))
-  {
-    return 0;
-  }
-  else
-  {
-    return nLen;
-  }
+    if (HAL_OK != HAL_UART_Receive(&huart2, nDat, nLen, 100))
+    {
+        return 0;
+    }
+    else
+    {
+        return nLen;
+    }
 }
 
 void vFTBus_Delay(void)
 {
-  HAL_Delay(1);
+    HAL_Delay(1);
 }
 /* USER CODE END 1 */
