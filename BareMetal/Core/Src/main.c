7a978b79ac6446ce0fd7de299213aeb423fb0732
Garliccurry <1095533196@qq.com>
2025-09-11 00:17:49 +0800

    [fix] fix the issue of inaccurate microsecond timer clock

--- BareMetal/Core/Src/main.c
+++ BareMetal/Core/Src/main.c
@@ -105,8 +105,8 @@ int main(void)
     MX_TIM3_Init();
     MX_TIM4_Init();
     MX_USART2_UART_Init();
-    MX_TIM5_Init();
     MX_TIM2_Init();
+    MX_TIM1_Init();
     /* USER CODE BEGIN 2 */
 
     Log_RegisterOutput(Usart_LogPrint);
--- BareMetal/Core/Src/tim.c
+++ BareMetal/Core/Src/tim.c
@@ -24,11 +24,47 @@
 
 /* USER CODE END 0 */
 
+TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
-TIM_HandleTypeDef htim5;
 
+/* TIM1 init function */
+void MX_TIM1_Init(void)
+{
+    /* USER CODE BEGIN TIM1_Init 0 */
+
+    /* USER CODE END TIM1_Init 0 */
+
+    TIM_ClockConfigTypeDef  sClockSourceConfig = {0};
+    TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+    /* USER CODE BEGIN TIM1_Init 1 */
+
+    /* USER CODE END TIM1_Init 1 */
+    htim1.Instance = TIM1;
+    htim1.Init.Prescaler = 168 - 1;
+    htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim1.Init.Period = 10 - 1;
+    htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim1.Init.RepetitionCounter = 0;
+    htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    if (HAL_TIM_Base_Init(&htim1) != HAL_OK) {
+        Error_Handler();
+    }
+    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+    if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN TIM1_Init 2 */
+
+    /* USER CODE END TIM1_Init 2 */
+}
 /* TIM2 init function */
 void MX_TIM2_Init(void)
 {
@@ -45,7 +81,7 @@ void MX_TIM2_Init(void)
     htim2.Instance = TIM2;
     htim2.Init.Prescaler = 84 - 1;
     htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim2.Init.Period = 100;
+    htim2.Init.Period = 100 - 1;
     htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
     if (HAL_TIM_Base_Init(&htim2) != HAL_OK) {
@@ -154,45 +190,23 @@ void MX_TIM4_Init(void)
     /* USER CODE END TIM4_Init 2 */
     HAL_TIM_MspPostInit(&htim4);
 }
-/* TIM5 init function */
-void MX_TIM5_Init(void)
-{
-    /* USER CODE BEGIN TIM5_Init 0 */
-
-    /* USER CODE END TIM5_Init 0 */
-
-    TIM_ClockConfigTypeDef  sClockSourceConfig = {0};
-    TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-    /* USER CODE BEGIN TIM5_Init 1 */
-
-    /* USER CODE END TIM5_Init 1 */
-    htim5.Instance = TIM5;
-    htim5.Init.Prescaler = 84 - 1;
-    htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim5.Init.Period = 1;
-    htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
-    if (HAL_TIM_Base_Init(&htim5) != HAL_OK) {
-        Error_Handler();
-    }
-    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-    if (HAL_TIM_ConfigClockSource(&htim5, &sClockSourceConfig) != HAL_OK) {
-        Error_Handler();
-    }
-    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-    if (HAL_TIMEx_MasterConfigSynchronization(&htim5, &sMasterConfig) != HAL_OK) {
-        Error_Handler();
-    }
-    /* USER CODE BEGIN TIM5_Init 2 */
-
-    /* USER CODE END TIM5_Init 2 */
-}
 
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *tim_baseHandle)
 {
-    if (tim_baseHandle->Instance == TIM2) {
+    if (tim_baseHandle->Instance == TIM1) {
+        /* USER CODE BEGIN TIM1_MspInit 0 */
+
+        /* USER CODE END TIM1_MspInit 0 */
+        /* TIM1 clock enable */
+        __HAL_RCC_TIM1_CLK_ENABLE();
+
+        /* TIM1 interrupt Init */
+        HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
+        /* USER CODE BEGIN TIM1_MspInit 1 */
+
+        /* USER CODE END TIM1_MspInit 1 */
+    } else if (tim_baseHandle->Instance == TIM2) {
         /* USER CODE BEGIN TIM2_MspInit 0 */
 
         /* USER CODE END TIM2_MspInit 0 */
@@ -205,19 +219,6 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *tim_baseHandle)
         /* USER CODE BEGIN TIM2_MspInit 1 */
 
         /* USER CODE END TIM2_MspInit 1 */
-    } else if (tim_baseHandle->Instance == TIM5) {
-        /* USER CODE BEGIN TIM5_MspInit 0 */
-
-        /* USER CODE END TIM5_MspInit 0 */
-        /* TIM5 clock enable */
-        __HAL_RCC_TIM5_CLK_ENABLE();
-
-        /* TIM5 interrupt Init */
-        HAL_NVIC_SetPriority(TIM5_IRQn, 7, 0);
-        HAL_NVIC_EnableIRQ(TIM5_IRQn);
-        /* USER CODE BEGIN TIM5_MspInit 1 */
-
-        /* USER CODE END TIM5_MspInit 1 */
     }
 }
 
@@ -292,7 +293,19 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef *timHandle)
 
 void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *tim_baseHandle)
 {
-    if (tim_baseHandle->Instance == TIM2) {
+    if (tim_baseHandle->Instance == TIM1) {
+        /* USER CODE BEGIN TIM1_MspDeInit 0 */
+
+        /* USER CODE END TIM1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM1_CLK_DISABLE();
+
+        /* TIM1 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(TIM1_UP_TIM10_IRQn);
+        /* USER CODE BEGIN TIM1_MspDeInit 1 */
+
+        /* USER CODE END TIM1_MspDeInit 1 */
+    } else if (tim_baseHandle->Instance == TIM2) {
         /* USER CODE BEGIN TIM2_MspDeInit 0 */
 
         /* USER CODE END TIM2_MspDeInit 0 */
@@ -304,18 +317,6 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *tim_baseHandle)
         /* USER CODE BEGIN TIM2_MspDeInit 1 */
 
         /* USER CODE END TIM2_MspDeInit 1 */
-    } else if (tim_baseHandle->Instance == TIM5) {
-        /* USER CODE BEGIN TIM5_MspDeInit 0 */
-
-        /* USER CODE END TIM5_MspDeInit 0 */
-        /* Peripheral clock disable */
-        __HAL_RCC_TIM5_CLK_DISABLE();
-
-        /* TIM5 interrupt Deinit */
-        HAL_NVIC_DisableIRQ(TIM5_IRQn);
-        /* USER CODE BEGIN TIM5_MspDeInit 1 */
-
-        /* USER CODE END TIM5_MspDeInit 1 */
     }
 }
 
@@ -354,7 +355,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
         Sensor_TimerGetSensor();
         SoftwareTimer_Update();
     }
-    if (htim->Instance == TIM5) {
+    if (htim->Instance == TIM1) {
         // Control_LeggedBalance();
         Info_UsTickIncrease(); // TODO(oujiali)时钟有问题
     }
